fabd09acef3d39c77ad688a764228df73ccc618a
Michal Hrusecky <michal.hrusecky@turris.com>
2021-07-21 10:51:26 +0200

    Turris polishing
    Addressing various small issues incompatible with Turris SW policies.

--- logger/alert_morce.lua
+++ logger/alert_morce.lua
@@ -1,65 +1,69 @@
--- ----------------------------------------------------------------------------
---
--- Copyright 2021 Michal Hrusecky <michal.hrusecky@turris.com>
---
--- This file is part of Morce, which is free software. It is made
--- available to you under the terms of the GNU General Public License
--- as published by the Free Software Foundation, either version 3 of
--- the License, or (at your option) any later version. For more
--- information, see COPYING.
---
--- ----------------------------------------------------------------------------
+--[[
 
--- this pulls in snort bindings with ffi
+Copyright 2021 CZ.NIC, z.s.p.o
+
+This file is part of Morce, which is free software. It is made
+available to you under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version. For more
+information, see COPYING.
+
+]]
+
+-- Include required modules
 require("snort_plugin")
-require("uci")
+local uci = require "uci"
+local sqlite = require "luasql.sqlite3"
 
--- Some globals
-db_con = false
-u_cur = false
+-- Globals to hold open sqlite and uci structures
+local db = false
+local db_con = false
+local u_cur = false
 
 function init()
     -- Initialize SQLite
-    driver = require "luasql.sqlite3"
-    db = driver.sqlite3()
-
-    if(not db) then
+    db = sqlite.sqlite3()
+    if not db then
         return false
     end
 
     -- Initialize UCI
-    u_cur = uci.cursor()
+    u_cur = uci.uci.cursor()
 
+    -- Open the database
     local db_path = u_cur:get("morce", "setup", "live_database") or "/tmp/morce-alerts.sqlite"
     db_con = db:connect(db_path)
-    if(not db_con) then
+    if not db_con then
         return false
     end
 
     -- WAL should be faster and better for concurrent access
     db_con:execute([[PRAGMA journal_mode = WAL]])
-    -- We also prefer to do DB maintenance manually
+    -- We also prefer to do DB maintenance manually as we only add data from snort plugin
     db_con:execute([[PRAGMA auto_vacuum = NONE]])
-    -- But we do like foreign keys
+    -- But we do need foreign keys for consistency
     db_con:execute([[PRAGMA foreign_keys = ON]])
 
     -- Create tables and view if they don't exist yet
-    db_con:execute([[CREATE TABLE IF NOT EXISTS alert_messages(
-        alert_id char(20) NOT NULL PRIMARY KEY ON CONFLICT REPLACE,
-        msg text default ""
-	) WITHOUT ROWID;]])
-    db_con:execute([[CREATE TABLE IF NOT EXISTS live_alerts(
-        time timestamp default current_timestamp,
-        alert_id varchar(20) default "" REFERENCES alert_messages(alert_id) ON DELETE RESTRICT,
-        mac varchar(20) default "",
-        dst_ip varchar(50) default "",
-        dst_port integer default 0
-	);]])
+    db_con:execute([[
+        CREATE TABLE IF NOT EXISTS alert_messages(
+            alert_id char(20) NOT NULL PRIMARY KEY ON CONFLICT REPLACE, msg text default ""
+	    ) WITHOUT ROWID;
+    ]])
+    db_con:execute([[
+        CREATE TABLE IF NOT EXISTS live_alerts(
+            time timestamp default current_timestamp,
+            alert_id varchar(20) default "" REFERENCES alert_messages(alert_id) ON DELETE RESTRICT,
+            mac varchar(20) default "",
+            dst_ip varchar(50) default "",
+            dst_port integer default 0
+	    );
+    ]])
     return true
 end
 
-function alert ()
-    -- get luajit structs
+function alert()
+    -- Get data from the event
     local evt = ffi.C.get_event()
     local pkt = ffi.C.get_packet()
     local alert_id = string.format("%d:%d:%d", evt.gid, evt.sid, evt.rev)
@@ -71,23 +75,26 @@ function alert ()
     print(string.format([['Security alert from host %s to %s:%d%s%s']],
                           src_eth, dst_ip, pkt.dp, " - ", msg))
     -- Log data into database
-    db_con:execute(string.format([[INSERT OR REPLACE INTO alert_messages VALUES ('%s','%s')]], alert_id, db_con:escape(msg)))
-    db_con:execute(string.format([[INSERT INTO live_alerts (alert_id,mac,dst_ip,dst_port) VALUES ('%s','%s','%s',%d)]],
-                                alert_id, db_con:escape(src_eth), db_con:escape(dst_ip), pkt.dp))
+    db_con:execute(string.format(
+        [[INSERT OR REPLACE INTO alert_messages VALUES ('%s','%s')]],
+        alert_id, db_con:escape(msg)))
+    db_con:execute(string.format(
+        [[INSERT INTO live_alerts (alert_id,mac,dst_ip,dst_port) VALUES ('%s','%s','%s',%d)]],
+        alert_id, db_con:escape(src_eth), db_con:escape(dst_ip), pkt.dp))
 
     -- Create a notification
     local notify = u_cur:get("morce", "notify", "enabled") or 1
-    if(notify == 1) then
+    if notify == 1 then
         local notify_cmd = u_cur:get("morce", "notify", "command") or "create_notification -s error"
-        os.execute(string.format([[%s 'Security alert from host %s to %s:%d%s%s']],
-                                   u_cur:get("morce", "notify", "command"), src_eth, dst_ip, pkt.dp, "\n", msg))
+        os.execute(string.format(
+            [[%s 'Security alert from host %s to %s:%d%s%s']],
+            u_cur:get("morce", "notify", "command"), src_eth, dst_ip, pkt.dp, "\n", msg))
     end
 end
 
--- plugin table is required
+-- plugin table to register ourselves in snort
 plugin = {
     type = "logger", 
     name = "alert_morce",
-    version = 1.0
+    version = "1.1"
 }
-
